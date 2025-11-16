72b12b52415912fc995ffe5d2011dd1ad27c8fba
Amir Makhmudov <amir.makhmudov.f@gmail.com>
2025-11-16 23:55:30 +0300

    feat: создать абстрактный класс Pet

---
+++ src/Pet.java
@@ -0,0 +1,21 @@
+public abstract class Pet {
+    private int pawsCount;
+
+    public Pet(int pawsCount) {
+        this.pawsCount = pawsCount;
+    }
+
+    public void sleep() {
+        System.out.println("Сплю");
+    }
+
+    public void play() {
+        System.out.println("Играю");
+    }
+
+    public abstract void giveVoice();
+
+    public int getPawsCount() {
+        return pawsCount;
+    }
+}
