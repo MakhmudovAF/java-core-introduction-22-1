9023f088d2df0f85b4909bd616acd3e778ea0e70
Amir Makhmudov <amir.makhmudov.f@gmail.com>
2025-11-16 23:58:45 +0300

    feat: создать класс Cat

---
+++ src/Cat.java
@@ -0,0 +1,14 @@
+public class Cat extends Pet {
+    public Cat() {
+        super(4);
+    }
+
+    @Override
+    public void giveVoice() {
+        System.out.println("Мяу!");
+    }
+
+    public void catchMouse() {
+        System.out.println("Поймала мышку!");
+    }
+}
