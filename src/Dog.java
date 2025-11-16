39f91e43bd9741e6dbab79cc68606594d48b873d
Amir Makhmudov <amir.makhmudov.f@gmail.com>
2025-11-16 23:58:15 +0300

    feat: создать класс Dog

---
+++ src/Dog.java
@@ -0,0 +1,14 @@
+public class Dog extends Pet {
+    public Dog() {
+        super(4);
+    }
+
+    @Override
+    public void giveVoice() {
+        System.out.println("Гав!");
+    }
+
+    public void bringStick() {
+        System.out.println("Принёс палочку, как хороший мальчик!");
+    }
+}
